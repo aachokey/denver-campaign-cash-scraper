6d1d11a23fc118018ac5d0e23d1552b50a4a2efb
Aric Chokey <achokey@Arics-MacBook-Pro.local>
2025-01-02 17:48:35 -0700

    Convert data to csv

---
+++ convert_csv.py
@@ -0,0 +1,25 @@
+import csv
+import json
+
+
+def main():
+    
+    DATA_FILE = "denver-contributions.json"
+    OUTPUT_FILE = "denver-contributions.csv"
+    
+    contribution_data = None
+    with open(DATA_FILE, "r") as df:
+        contribution_data = json.load(df)
+    
+    
+    contribution_data = contribution_data["searchContributionTransactions"]
+    
+    with open(OUTPUT_FILE) as outfile:
+        fieldnames = contribution_data[0].keys()
+        writer = csv.DictWriter(outfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(contribution_data)
+            
+
+if __name__ == "__main__":
+    main()
